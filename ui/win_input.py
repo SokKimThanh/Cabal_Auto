aa68e962bae5af2a043657a6e10a2c168586b134
SokKimThanh <thanhsk1991@gmail.com>
2025-10-24 22:36:13 +0700

    Remove legacy shim modules for template matching and Windows input. These files were previously used to facilitate legacy imports but are no longer necessary.

--- ui/win_input.py
+++
@@ -1,5 +0,0 @@
-# Shim module to allow legacy imports: from win_input import ...
-try:
-    from lib.system.win_input import *  # noqa: F401,F403
-except Exception:  # pyright: ignore[reportMissingImports]
-    pass
