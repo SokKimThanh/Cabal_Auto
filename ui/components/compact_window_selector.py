9268c8649c8c930eecc20a9723f6a65f9434cf7f
SokKimThanh <thanhsk1991@gmail.com>
2026-09-07 13:51:02 +0700

    fix font size

--- ui/components/compact_window_selector.py
+++ ui/components/compact_window_selector.py
@@ -249,7 +249,7 @@ class CompactWindowSelector:
                 height=6,
                 width=50,
                 yscrollcommand=scrollbar.set,
-                font=UI.FONT_MONO,
+                font=UI.FONT_BODY,
                 bg=UI.BG_BASE,
                 fg=UI.TEXT_PRIMARY,
                 selectmode="single",
